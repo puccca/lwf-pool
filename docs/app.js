2f83e95bf391cba871f8cb910f4c743581169581
puccca <32229393+puccca@users.noreply.github.com>
2018-02-03 15:21:20 +0200

    Update app.js

--- docs/app.js
+++ docs/app.js
@@ -4,23 +4,25 @@ app.controller('indexCtrl', function($scope, $http) {
     $scope.accounts = [];
     $scope.lastpayout = 0;
     $scope.nextpayout = 0;
+    $scope.weightvote = 0;    
 
     $http.get ('poollogs.json').then (function (res) {
         $scope.lastpayout = res.data.lastpayout * 1000;
         $scope.nextpayout = moment ($scope.lastpayout).add (1, 'week').valueOf();
+        $scope.totalweight = res.data.totalweight;
+        $scope.totalpaid = res.data.totalpaid;
+        $scope.totalpending = res.data.totalpending;
         $scope.accounts = [];
-        $scope.total = { paid: 0.0, pending: 0.0 };
 
         for (addr in res.data.accounts) {
             var it = res.data.accounts[addr];
             it['address'] = addr;
             $scope.accounts.push (it);
-            $scope.total.paid += res.data.accounts[addr].received;
-            $scope.total.pending += res.data.accounts[addr].pending;
         }
     });
 
-    $http.get ('https://wallet.lisknode.io/api/delegates/get?username=dakk').then (function (res) {
+    $http.get ('https://wallet.lisknode.io/api/delegates/get?username=YOURDELEGATE').then (function (res) {
         $scope.delegate = res.data.delegate;
+        $scope.weightvote = res.data.delegate.vote/100000000;        
     });
 });
